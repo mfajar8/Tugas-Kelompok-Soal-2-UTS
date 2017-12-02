bcb4f283ff790f10e2416466324480a44d43790e
LENOVO <LENOVO@MFAJAR8>
2017-12-02 18:49:25 +0700

    Tugas Kelompok Soal 2 UTS
    Mochamad Fajar Ismatulloh
    Alfa Julianta Ginting
    Gerdi Abdi Salam

---
+++ src/Model/AnggotaGym.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class AnggotaGym {
+    private String noKTP;
+    private String nama;
+    private String kelamin;
+    private String cabang;
+    private String handuk;
+    private String paket;
+    private int biaya;
+
+    public String getNoKTP() {
+        return noKTP;
+    }
+
+    public void setNoKTP(String noKTP) {
+        this.noKTP = noKTP;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getKelamin() {
+        return kelamin;
+    }
+
+    public void setKelamin(String kelamin) {
+        this.kelamin = kelamin;
+    }
+
+    public String getCabang() {
+        return cabang;
+    }
+
+    public void setCabang(String cabang) {
+        this.cabang = cabang;
+    }
+
+    public String getHanduk() {
+        return handuk;
+    }
+
+    public void setHanduk(String handuk) {
+        this.handuk = handuk;
+    }
+
+    public String getPaket() {
+        return paket;
+    }
+
+    public void setPaket(String paket) {
+        this.paket = paket;
+    }
+
+    public int getBiaya() {
+        return biaya;
+    }
+
+    public void setBiaya(int biaya) {
+        this.biaya = biaya;
+    }
+    
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
